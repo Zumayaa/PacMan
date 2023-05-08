5d71439c87f8e8aa41991dec3f4f41e082179b7a
Zumayaa <abzu_21@alu.uabcs.mx>
2023-05-07 17:28:23 -0700

    Se agregó la música

--- src/Splash.java
+++ src/Splash.java
@@ -1,7 +1,14 @@
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
 public class Splash extends JDialog {
 
@@ -99,7 +106,10 @@ public class Splash extends JDialog {
             evadeTitle.setBounds(50,50,300,300);
             add(evadeTitle);
             
-             
+            //MUSICA SE REPRODUCE
+            ReproductorMusica reproductor = new ReproductorMusica();
+            reproductor.reproducir("musica/music.wav"); 
+            //reproductor.detener();
             
             JLabel point1 = new JLabel("");
             point1.setSize(20,20);
@@ -204,6 +214,25 @@ public class Splash extends JDialog {
 
             add(atras);
         
+        }
+    }
+    //clase para la música
+    public class ReproductorMusica {
+        private Clip clip;
+
+        public void reproducir(String rutaArchivo) {
+            try {
+                clip = AudioSystem.getClip();
+                AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(rutaArchivo));
+                clip.open(inputStream);
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+            } catch (LineUnavailableException | UnsupportedAudioFileException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void detener() {
+            clip.stop();
         }
     }
 
